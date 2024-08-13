24e8870e8584fde995354b392640ab1d3a2351ac
MudasirGul <mudassirgulshair@gmail.com>
2024-08-13 14:44:42 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,35 @@
+
+import React from 'react'
+import St from "./assets/components/1st"
+import Sl from "./assets/components/3rd"
+import Navbar from "./assets/components/2nd"
+import Card from "./assets/components/4th"
+import Card2 from "./assets/components/5th"
+import Card3 from "./assets/components/6th"
+import Card4 from "./assets/components/7th"
+import Card5 from "./assets/components/8th"
+import Container from "./assets/components/9th"
+import Links from "./assets/components/10th"
+//import Login from "./assets/components/11th"
+
+const App = () => {
+  return (
+    <div>
+      <St></St>
+      <Navbar></Navbar>
+      <Sl></Sl>
+      <Card></Card>
+      <Card2></Card2>
+      <Card3></Card3>
+      <Card4></Card4>
+      <Card5></Card5>
+      <Container></Container>
+      <Links></Links>
+      
+      
+      
+    </div>
+  )
+}
+
+export default App
